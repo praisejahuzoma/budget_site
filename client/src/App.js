0037de81683ccef31a99fe06d3c3bc83177766c5
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-09-23 16:43:20 +0100

    feat(app): add Add component to the main App

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+// src/App.js
+import React from 'react';
+import Add from './pages/add';
+
+function App() {
+  return (
+    <div className="App">
+      <Add/>
+    </div>
+  );
+}
+
+export default App;
